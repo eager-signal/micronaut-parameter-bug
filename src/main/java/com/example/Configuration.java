c5ddce1d8e1e343fbf6407df7d52dca81e59b508
Chris Eager <chris@signal.org>
2024-07-03 10:48:12 -0500

    Update to Micronaut 4

--- src/main/java/com/example/Configuration.java
+++ src/main/java/com/example/Configuration.java
@@ -3,8 +3,8 @@ package com.example;
 import io.micronaut.context.annotation.EachProperty;
 import io.micronaut.context.annotation.Parameter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 @EachProperty("config")
